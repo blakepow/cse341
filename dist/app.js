b0b0ae2affc7883d768a9b516b49a8cf0714e5ac
blakepow <blakepow@gmail.com>
2022-11-02 19:59:33 -0600

    procfile added

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,7 @@
 "use strict";
 var express = require('express');
 var app = express();
-var port = 4000;
+var port = process.env.PORT || 4000;
 app.use('/', require('./routes/userRoute'));
 app.listen(port, function () {
     console.log("Server is running on port " + port);
