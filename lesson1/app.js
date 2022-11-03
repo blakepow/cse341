a6567480031c4a229744a2bf061b09b451108efd
blakepow <blakepow@gmail.com>
2022-11-02 19:30:50 -0600

    npm start runs node app.js

--- lesson1/app.js
+++ lesson1/app.js
@@ -1,9 +1,10 @@
 const app = require('express')();
+const port = 4000;
 
 app.get('/', (req, res) => {
     res.send('Blake Powelson');
 })
 
-app.listen(4000, () => {
-    console.log('Listening on port 4000');
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
